29c9a67ab26b1b27e6dac2b369ab7581843878b7
Udhaykrishn <krishnauday320@gmail.com>
2024-07-19 14:00:55 +0530

    update the app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
 import { BookModule } from './book/book.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver,ApolloDriverConfig } from '@nestjs/apollo';
+
 
 @Module({
-  imports: [PrismaModule, BookModule]
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver:ApolloDriver,
+    autoSchemaFile:true,
+  }),
+BookModule]
 })
 export class AppModule {}
