d53aa607f6cd4d13e1efded8c4017252d8154cc7
Udhaykrishn <krishnauday320@gmail.com>
2024-07-19 13:56:46 +0530

    added the resolver graphql methods

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Resolver,Query,Mutation,Args,ID } from '@nestjs/graphql';
+import { Book } from "./models/book.model";
+import { BookService } from './book.service';
+import { CreateBookInput } from './dto/create-book.input';
+import { UpdateBookInput } from './dto/update-book.input';
 
-@Controller('book')
-export class BookResolver {}
+
+@Resolver(()=>Book)
+export class BookResolver {
+    constructor(private readonly bookService:BookService){}
+
+    @Query(()=>[Book],{name:'books'})
+    async findAll(){
+        return this.bookService.findAll()
+    }
+
+    @Query(()=>Book,{name:'book'})
+    async findOne(@Args('id',{type:()=>ID}) id:string){
+        return this.bookService.findOne(id)
+    }
+
+    @Mutation(()=>Book)
+    async createBook(@Args('createBookInput') createBookInput:CreateBookInput ){
+        return this.bookService.create(createBookInput)
+    }
+
+    @Mutation(()=>Book)
+    async updateBook(
+        @Args('id',{type:()=>ID})id:string,
+        @Args('updateBookInput') updateBookInput:UpdateBookInput
+    ){
+        return this.bookService.update(id,updateBookInput)
+    }
+
+    @Mutation(()=>Book)
+    async deleteBook(
+        @Args('id',{type:()=>ID}) id:string
+    ){
+        return this.bookService.delete(id)
+    }
+}
