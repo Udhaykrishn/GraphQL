a048aba9b211d8bde3baafe6e3d0a6388a67e5fc
Udhaykrishn <krishnauday320@gmail.com>
2024-07-19 13:13:41 +0530

    databased connectection code is done

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Global, Injectable,OnModuleDestroy,OnModuleInit,Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+@Global()
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy {
+    
+    private logger = new Logger(PrismaService.name)
+
+    async onModuleInit(){
+        await this.$connect()
+        this.logger.log("Database Connected")
+    }
+    
+    async onModuleDestroy(){
+        await this.$disconnect()
+        this.logger.log("Database Disconnected")
+    }
+}
